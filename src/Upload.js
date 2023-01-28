dfc5d45d2f06c619e3ec0a286f177a5004527e53
Keval Khatri <kevalkhatri32@gmail.com>
2023-01-28 12:44:14 +0530

    Replace react-aws-s3 with react-s3, request being forwarded after replacing

--- src/Upload.js
+++ src/Upload.js
@@ -1,29 +1,41 @@
 import React, { useState } from "react";
-import S3 from "react-aws-s3";
+// import S3 from "react-aws-s3";
+import S3FileUpload from "react-s3/lib/ReactS3";
 
+window.Buffer = window.Buffer || require("buffer").Buffer;
 // a React functional component, used to create a simple upload input and button
 
 const Upload = () => {
   const [selectedFile, setSelectedFile] = useState(null);
 
   // the configuration information is fetched from the .env file
+  console.log(process.env.REACT_APP_BUCKET_NAME);
   const config = {
     bucketName: process.env.REACT_APP_BUCKET_NAME,
     region: process.env.REACT_APP_REGION,
-    accessKeyId: process.env.REACT_APP_ACCESS,
-    secretAccessKey: process.env.REACT_APP_SECRET,
+    accessKeyId: process.env.REACT_APP_ACCESS_KEY,
+    secretAccessKey: process.env.REACT_APP_SECRET_KEY,
   };
 
   const handleFileInput = (e) => {
+    console.log(e.target.files[0]);
     setSelectedFile(e.target.files[0]);
   };
 
   const uploadFile = async (file) => {
-    const ReactS3Client = new S3(config);
+    // const ReactS3Client = new S3(config);
     // the name of the file uploaded is used to upload it to S3
-    ReactS3Client.uploadFile(file, file.name)
-      .then((data) => console.log(data.location))
-      .catch((err) => console.error(err));
+    // ReactS3Client.uploadFile(file, file.name)
+    //   .then((data) => console.log(data.location))
+    //   .catch((err) => console.error(err));
+
+    S3FileUpload.uploadFile(file, config)
+      .then((data) => {
+        console.log(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
   return (
     <div>
